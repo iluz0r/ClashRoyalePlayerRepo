fe6387e20fd3838d128f604755db770430345476
mpolonioli <m.polonioli@campus.unimib.it>
2017-05-26 14:55:30 +0200

    eliminata funzione getCoord in main

--- ClashRoyalePlayer/src/clashroyaleplayer/app/Main.java
+++ ClashRoyalePlayer/src/clashroyaleplayer/app/Main.java
@@ -1,25 +1,15 @@
 package clashroyaleplayer.app;
 
-import java.awt.MouseInfo;
-import java.awt.Point;
-import java.awt.Robot;
-
 import clashroyaleplayer.core.AutoPlayer;
 
 public class Main {
 	
 	public static void main(String[] args) throws Exception {
 		
-		/*
 		AutoPlayer player = new AutoPlayer();
 		player.start();
-		*/
 		
-		Robot robot = new Robot();
-		robot.delay(10000);
-		Point coord = MouseInfo.getPointerInfo().getLocation();
-		System.out.println("X: " + coord.getX() + " Y: " + coord.getY());
-		
+
 	}
 
 }
