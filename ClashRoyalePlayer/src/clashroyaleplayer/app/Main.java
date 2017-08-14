d78d0c2fa4a14148d0b7e17396c7fcf3d74911b1
Angelo <Angelo@PC-Angelo.lan>
2017-08-14 13:53:54 +0200

    Refactor

--- ClashRoyalePlayer/src/clashroyaleplayer/app/Main.java
+++ ClashRoyalePlayer/src/clashroyaleplayer/app/Main.java
@@ -26,10 +26,11 @@ public class Main {
 			// Set the focus on Nox window
 			User32.INSTANCE.SetForegroundWindow(noxHandle);
 
-			//findPosInWnd();
-			//ImageCapturer capt = new ImageCapturer();
-			//BufferedImage img = capt.captureImage(new Rectangle(140, 470, 196, 135));
-			//capt.saveImage(img, "images/", "collectedCrownChest", "jpg");
+			// findPosInWnd();
+			// ImageCapturer capt = new ImageCapturer();
+			// BufferedImage img = capt.captureImage(new Rectangle(140, 470,
+			// 196, 135));
+			// capt.saveImage(img, "images/", "collectedCrownChest", "jpg");
 
 			AutoPlayer player = new AutoPlayer();
 			player.start();
